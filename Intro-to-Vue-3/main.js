56d1718c25fae145c3ee6d7f9fb080a52897cc16
Darryl Le Roux <le.roux.darryl@gmail.com>
2021-01-19 19:39:53 +0200

    for in loops through variants

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -2,7 +2,18 @@ const app = Vue.createApp({
   data() {
     return {
       product: "socks",
-      description: "Kiff toe warmers",
+      image: "./assets/images/socks_green.jpg",
+      inventory: 100,
+      onSale: false,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
+      sizes: [
+        { id: 1234, size: "12" },
+        { id: 1245, size: "13" },
+      ],
     };
   },
 });
